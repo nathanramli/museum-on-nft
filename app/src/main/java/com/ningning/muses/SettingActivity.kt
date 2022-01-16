7d47ae3091651127ba539a4647f696e9748ab530
Varrel Marcellius <marcellius33@gmail.com>
2022-01-17 01:19:48 +0700

    feat: add logout dialog

--- app/src/main/java/com/ningning/muses/SettingActivity.kt
+++ app/src/main/java/com/ningning/muses/SettingActivity.kt
@@ -1,8 +1,12 @@
 package com.ningning.muses
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class SettingActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
@@ -25,6 +29,20 @@ class SettingActivity : AppCompatActivity() {
     }
 
     private fun setupListener() {
-
+        val logout = findViewById<TextView>(R.id.logout)
+        logout.setOnClickListener {
+            val bottomSheet = BottomSheetDialog(this)
+            val view = layoutInflater.inflate(R.layout.fragment_bottom_sheet_logout, null)
+            bottomSheet.setContentView(view)
+            view.findViewById<Button>(R.id.noLogoutButton)?.setOnClickListener {
+                bottomSheet.dismiss()
+            }
+            view.findViewById<Button>(R.id.yesLogoutButton)?.setOnClickListener {
+                finishAffinity()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            bottomSheet.show()
+        }
     }
 }
